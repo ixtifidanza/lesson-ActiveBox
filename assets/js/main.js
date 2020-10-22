31f4b6a542ce51c1e9280fef6c762e5184fb7366
ixtifidanza <ixtifidanza@gmail.com>
2020-10-22 16:11:42 +0500

    Fineshed Proekt

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,12 @@
 $ (function () {     
 
-
+        /* Fixed header */
     let header = $('#header');
     let intro = $('#intro');
-    let introH;
+    let introH; 
     let scrollPos = $(window).scrollTop();
+    let nav = $('#nav');
+    let navToggle = $('#navToggle');
 
     $(window).on('scroll load resize    ', function () {
         let introH = intro.innerHeight();
@@ -16,4 +18,44 @@ $ (function () {
             header.removeClass('fixed');    
         }
     });
+
+
+    /* Smooth Scroll */
+
+    $('[data-scroll]').on('click', function(event) {
+        event.preventDefault();
+
+        let blockId = $(this).data('scroll');
+        let blockOffset = $(blockId).offset().top;
+
+        nav.removeClass('show');
+
+        $('html, body').animate({
+            scrollTop: blockOffset -70
+        }, 800);
+    });
+
+
+
+    /* Nav Toggle  */
+    navToggle.on('click', function(event) {
+        event.preventDefault();
+        nav.toggleClass('show');
+
+    });
+
+
+    /* Reviews */
+        let slider = $('#reviewsSlider');
+
+        slider.slick({
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            fade: false,
+            arrows: false,
+            dots: true    
+          });
+
+
  });
